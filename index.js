789df280ac541cba19d4265eb2e38916e8cf8eb3
Edenilton <edeniltonborba.adm@gmail.com>
2020-04-18 14:33:52 +0200

    servidor

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+
+const schema = require('./schema');
 
 const app = express();
 const PORT = 3000;
+
+app.use('./graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}));
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`)
